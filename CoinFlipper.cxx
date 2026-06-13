9d632e589f9ba0baae7f13f89ea8eba6376dcc95
Andreaphus <151429538+Andreaphus@users.noreply.github.com>
2026-06-13 21:04:12 +0530

    Add files via upload

---
+++ CoinFlipper.cxx
@@ -0,0 +1,36 @@
+
+#include <iostream>
+class coin{
+public:	
+int engine()
+{
+int Randnum;
+srand(time(0));
+Randnum=rand()%2;	
+std::cout<<Randnum;
+return Randnum;
+}
+
+void result()
+{
+int check=engine();	
+
+if(check==0)
+{
+std::cout<<"Heads!"<<std::endl;	
+}
+
+if(check==1)
+{
+std::cout<<"Tails!"<<std::endl;	
+}
+
+}	
+};
+int main()
+{
+coin c;
+c.result();
+return 0;
+}
+
